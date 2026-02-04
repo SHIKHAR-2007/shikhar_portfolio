fcbac1f5773beee87f7e53868526fdd809aaf25d
Shikhar <ssbiology26@gmail.com>
2026-02-04 23:13:58 +0530

    LastUpdateToday31/01/2026

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // view engine
 app.set("view engine", "ejs");
